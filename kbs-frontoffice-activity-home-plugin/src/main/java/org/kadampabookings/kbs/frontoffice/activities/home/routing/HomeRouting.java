09cc9f89df07c17b83082a22f221f92c5db6ae9a
Bruno Salmon <dev.salmonb@gmail.com>
2023-11-15 14:28:01 +0000

    Removed "app-" prefix from front-office routes

--- kbs-frontoffice-activity-home-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/activities/home/routing/HomeRouting.java
+++ kbs-frontoffice-activity-home-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/activities/home/routing/HomeRouting.java
@@ -1,7 +1,7 @@
 package org.kadampabookings.kbs.frontoffice.activities.home.routing;
 
 public final class HomeRouting {
-    private final static String PATH = "/app-home";
+    private final static String PATH = "/home";
 
     public static String getPath() {
         return PATH;
