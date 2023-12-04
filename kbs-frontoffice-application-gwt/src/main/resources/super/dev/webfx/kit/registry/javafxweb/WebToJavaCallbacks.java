f82bca4cadb61b52121c445d1aca020d27e09260
Bruno Salmon <dev.salmonb@gmail.com>
2023-12-04 11:28:15 +0000

    Updated WebToJavaCallbacks GWT super source

--- kbs-frontoffice-application-gwt/src/main/resources/super/dev/webfx/kit/registry/javafxweb/WebToJavaCallbacks.java
+++ kbs-frontoffice-application-gwt/src/main/resources/super/dev/webfx/kit/registry/javafxweb/WebToJavaCallbacks.java
@@ -10,8 +10,7 @@ public class WebToJavaCallbacks {
     public native static void bindCallbackMethods(Object javaInstance, String javaClassName) /*-{
         switch (javaClassName) {
             case 'one.modality.event.frontoffice.activities.booking.views.DynamicMapView':
-                javaInstance.updateMapPosition = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::updateMapPosition().bind(javaInstance);
-                javaInstance.updateMarkers = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::updateMarkers().bind(javaInstance);
+                javaInstance.onGoogleMapLoaded = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::onGoogleMapLoaded().bind(javaInstance);
                 javaInstance.onMarkerClicked = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::onMarkerClicked(Lone/modality/event/frontoffice/activities/booking/views/MapMarker;).bind(javaInstance);
                 break;
         }
