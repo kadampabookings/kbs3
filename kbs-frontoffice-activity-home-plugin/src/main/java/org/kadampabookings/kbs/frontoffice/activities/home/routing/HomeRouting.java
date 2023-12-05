83fd147ac1f0c8aca4f6c6e1c34b9871fbf775be
Bruno Salmon <dev.salmonb@gmail.com>
2023-12-05 17:15:19 +0000

    Removed kbs-frontoffice-activity-home-plugin module

--- kbs-frontoffice-activity-home-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/activities/home/routing/HomeRouting.java
+++
@@ -1,9 +0,0 @@
-package org.kadampabookings.kbs.frontoffice.activities.home.routing;
-
-public final class HomeRouting {
-    private final static String PATH = "/home";
-
-    public static String getPath() {
-        return PATH;
-    }
-}
