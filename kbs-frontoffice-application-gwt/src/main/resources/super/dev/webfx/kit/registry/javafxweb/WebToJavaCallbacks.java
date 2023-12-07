4d27071299ee9f9647fda58626b913e7cd2678d2
Bruno Salmon <dev.salmonb@gmail.com>
2023-12-07 20:39:38 +0000

    Made front-office booking page work on mobiles (tested on iOS)

--- kbs-frontoffice-application-gwt/src/main/resources/super/dev/webfx/kit/registry/javafxweb/WebToJavaCallbacks.java
+++ kbs-frontoffice-application-gwt/src/main/resources/super/dev/webfx/kit/registry/javafxweb/WebToJavaCallbacks.java
@@ -10,6 +10,9 @@ public class WebToJavaCallbacks {
     public native static void bindCallbackMethods(Object javaInstance, String javaClassName) /*-{
         switch (javaClassName) {
             case 'one.modality.event.frontoffice.activities.booking.views.DynamicMapView':
+                javaInstance.consoleLog = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::consoleLog(Ljava/lang/String;).bind(javaInstance);
+                javaInstance.consoleWarn = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::consoleWarn(Ljava/lang/String;).bind(javaInstance);
+                javaInstance.consoleError = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::consoleError(Ljava/lang/String;).bind(javaInstance);
                 javaInstance.onGoogleMapLoaded = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::onGoogleMapLoaded().bind(javaInstance);
                 javaInstance.onMarkerClicked = javaInstance.@one.modality.event.frontoffice.activities.booking.views.DynamicMapView::onMarkerClicked(Lone/modality/event/frontoffice/activities/booking/views/MapMarker;).bind(javaInstance);
                 break;
