32af86029927951b7a78b0f9119c35ccffd75a2c
Bruno Salmon <dev.salmonb@gmail.com>
2024-04-18 20:58:28 +0100

    Ran 'webfx update'

--- kbs-server-application-vertx/src/main/java/module-info.java
+++ kbs-server-application-vertx/src/main/java/module-info.java
@@ -13,7 +13,6 @@ module kbs.server.application.vertx {
     requires modality.ecommerce.payment.delegated.server;
     requires modality.ecommerce.payment.direct.server;
     requires webfx.platform.ast.factory.vertx;
-    requires webfx.platform.blob.java;
     requires webfx.platform.boot.vertx;
     requires webfx.platform.console.java;
     requires webfx.platform.fetch.vertx;
