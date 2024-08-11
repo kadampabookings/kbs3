1ba9c7843dda7001cc7aa246cdcf7868446bf10f
Bruno Salmon <dev.salmonb@gmail.com>
2024-08-11 22:48:10 +0100

    Committed missing files in kbs-frontoffice-mediaview

---
+++ kbs-frontoffice-mediaview/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module kbs.frontoffice.mediaview {
+
+    // Direct dependencies modules
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires modality.base.frontoffice.utility;
+    requires modality.base.shared.entities;
+    requires webfx.extras.imagestore;
+    requires webfx.extras.panes;
+    requires webfx.extras.player;
+    requires webfx.extras.player.audio.media;
+    requires webfx.extras.player.video;
+    requires webfx.extras.player.video.web.wistia;
+    requires webfx.kit.util;
+    requires webfx.platform.util;
+
+    // Exported packages
+    exports org.kadampabookings.kbs.frontoffice.mediaview;
+
+}
