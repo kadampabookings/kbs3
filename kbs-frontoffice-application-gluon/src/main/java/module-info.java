a370dbdc3b969c1139351aac9044f5bcfb24ece1
David <david.dev.kbs@kadampa.net>
2024-12-30 15:43:40 +0000

    Initial international dictionaries creation

--- kbs-frontoffice-application-gluon/src/main/java/module-info.java
+++ kbs-frontoffice-application-gluon/src/main/java/module-info.java
@@ -13,6 +13,8 @@ module kbs.frontoffice.application.gluon {
     requires modality.ecommerce.document.service.remote;
     requires modality.ecommerce.payment.buscall;
     requires modality.ecommerce.payment.remote;
+    requires webfx.extras.canvas.blob.openjfx;
+    requires webfx.extras.filepicker.openjfx;
     requires webfx.extras.visual.grid.peers.openjfx;
     requires webfx.extras.webtext.peers.openjfx;
     requires webfx.kit.javafxgraphics.openjfx;
