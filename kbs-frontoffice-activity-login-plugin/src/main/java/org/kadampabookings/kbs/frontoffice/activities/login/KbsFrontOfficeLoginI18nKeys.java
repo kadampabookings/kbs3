b6259623e13d702324db196cfa730a7b54f4c57a
Bruno Salmon <dev.salmonb@gmail.com>
2025-05-22 20:17:16 +0100

    Added a help text on the kbs login page (missing files)

---
+++ kbs-frontoffice-activity-login-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/activities/login/KbsFrontOfficeLoginI18nKeys.java
@@ -0,0 +1,8 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+package org.kadampabookings.kbs.frontoffice.activities.login;
+
+public interface KbsFrontOfficeLoginI18nKeys {
+
+    String KbsLoginHelp = "KbsLoginHelp";
+
+}
