3fed583e29ad5bc4298838f6e8b4b49a01728313
Bruno Salmon <dev.salmonb@gmail.com>
2025-08-12 14:37:39 +0100

    Considered changes in EntityStore cache API

--- kbs-client-festivaltypes/src/main/java/org/kadampabookings/kbs/client/festivaltypes/FXFestivals.java
+++ kbs-client-festivaltypes/src/main/java/org/kadampabookings/kbs/client/festivaltypes/FXFestivals.java
@@ -4,7 +4,6 @@ import dev.webfx.platform.console.Console;
 import dev.webfx.platform.uischeduler.UiScheduler;
 import dev.webfx.platform.util.collection.Collections;
 import dev.webfx.stack.cache.client.LocalStorageCache;
-import dev.webfx.stack.orm.entity.EntityList;
 import dev.webfx.stack.orm.entity.EntityStore;
 import dev.webfx.stack.orm.entity.EntityStoreQuery;
 import javafx.beans.property.ObjectProperty;
@@ -89,11 +88,11 @@ public final class FXFestivals {
 
     public static ReadOnlyObjectProperty<Event> lastFestivalProperty(FestivalType festivalType, boolean online) {
         if (festivalType != null) {
-            switch (festivalType) {
-                case SPRING_FESTIVAL: return online ? lastOnlineSpringFestivalProperty() : lastSpringFestivalProperty();
-                case SUMMER_FESTIVAL: return online ? lastOnlineSummerFestivalProperty() : lastSummerFestivalProperty();
-                case FALL_FESTIVAL  : return online ? lastOnlineFallFestivalProperty()   : lastFallFestivalProperty();
-            }
+            return switch (festivalType) {
+                case SPRING_FESTIVAL -> online ? lastOnlineSpringFestivalProperty() : lastSpringFestivalProperty();
+                case SUMMER_FESTIVAL -> online ? lastOnlineSummerFestivalProperty() : lastSummerFestivalProperty();
+                case FALL_FESTIVAL   -> online ? lastOnlineFallFestivalProperty()   : lastFallFestivalProperty();
+            };
         }
         return null;
     }
@@ -101,26 +100,22 @@ public final class FXFestivals {
     private static void loadLastFestivals() {
         LocalDate nextYear = Year.of(LocalDate.now().getYear() + 1).atDay(1);
         String select = "select name,type.name,startDate,endDate,kbs3,state,live,openingDate,bookingFormUrl from Event where type in (?) and startDate < ? order by startDate desc, name like '%Online%' ? 1 : 0 limit 2";
-        EntityStore.create().executeCachedQueryBatch(
-                LocalStorageCache.get().getCacheEntry("cache-last-festivals"), FXFestivals::onLastFestivalsLoaded,
+        EntityStore.create().executeQueryBatchWithCache(
+                LocalStorageCache.get().getCacheEntry("cache-last-festivals"),
                 new EntityStoreQuery(select, new Object[]{FestivalType.SPRING_FESTIVAL.getTypeId(), nextYear}),
                 new EntityStoreQuery(select, new Object[]{FestivalType.SUMMER_FESTIVAL.getTypeId(), nextYear}),
                 new EntityStoreQuery(select, new Object[]{FestivalType.FALL_FESTIVAL.getTypeId(), nextYear}))
             .onFailure(Console::log)
-            .onSuccess(FXFestivals::onLastFestivalsLoaded);
-    }
-
-    private static void onLastFestivalsLoaded(EntityList[] festivalsLists) {
-        UiScheduler.runInUiThread(() -> {
-            LAST_SPRING_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[0], 0)); // Spring Festival
-            LAST_SUMMER_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[1], 0)); // Summer Festival
-            LAST_FALL_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[2], 0)); // Fall Festival
-            LAST_FESTIVALS.setAll(getLastSpringFestival(), getLastSummerFestival(), getLastFallFestival());
-            LAST_ONLINE_SPRING_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[0], 1)); // Spring Festival
-            LAST_ONLINE_SUMMER_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[1], 1)); // Summer Festival
-            LAST_ONLINE_FALL_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[2], 1));  // Fall Festival
-            LAST_FESTIVALS.setAll(getLastSpringFestival(), getLastSummerFestival(), getLastFallFestival());
-        });
+            .onSuccess(festivalsLists -> UiScheduler.runInUiThread(() -> {
+                LAST_SPRING_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[0], 0)); // Spring Festival
+                LAST_SUMMER_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[1], 0)); // Summer Festival
+                LAST_FALL_FESTIVAL_PROPERTY  .set((Event) Collections.get(festivalsLists[2], 0)); // Fall Festival
+                LAST_FESTIVALS.setAll(getLastSpringFestival(), getLastSummerFestival(), getLastFallFestival());
+                LAST_ONLINE_SPRING_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[0], 1)); // Spring Festival
+                LAST_ONLINE_SUMMER_FESTIVAL_PROPERTY.set((Event) Collections.get(festivalsLists[1], 1)); // Summer Festival
+                LAST_ONLINE_FALL_FESTIVAL_PROPERTY  .set((Event) Collections.get(festivalsLists[2], 1));  // Fall Festival
+                LAST_FESTIVALS.setAll(getLastSpringFestival(), getLastSummerFestival(), getLastFallFestival());
+            }));
     }
 
     public static void init() {
