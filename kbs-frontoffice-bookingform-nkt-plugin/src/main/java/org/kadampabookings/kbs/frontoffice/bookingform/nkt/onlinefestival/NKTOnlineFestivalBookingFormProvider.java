60ed11e1015b9c241534f7d9434f2f5aefe3bcde
David Hello <david.dev.kbs@kadampa.net>
2025-12-14 12:52:40 +0000

    Changed to compile with the new Booking From style

--- kbs-frontoffice-bookingform-nkt-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/bookingform/nkt/onlinefestival/NKTOnlineFestivalBookingFormProvider.java
+++ kbs-frontoffice-bookingform-nkt-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/bookingform/nkt/onlinefestival/NKTOnlineFestivalBookingFormProvider.java
@@ -3,6 +3,7 @@ package org.kadampabookings.kbs.frontoffice.bookingform.nkt.onlinefestival;
 import one.modality.base.shared.entities.Event;
 import one.modality.booking.client.workingbooking.HasWorkingBookingProperties;
 import one.modality.booking.frontoffice.bookingform.BookingForm;
+import one.modality.booking.frontoffice.bookingform.BookingFormEntryPoint;
 import one.modality.booking.frontoffice.bookingform.BookingFormProvider;
 import one.modality.event.frontoffice.activities.book.event.EventBookingFormSettingsBuilder;
 import one.modality.event.frontoffice.eventheader.EventTitleHeader;
@@ -14,8 +15,12 @@ import org.kadampabookings.kbs.client.festivaltypes.FestivalType;
 public final class NKTOnlineFestivalBookingFormProvider implements BookingFormProvider {
 
     @Override
-    public boolean acceptEvent(Event event) {
-        return FestivalType.isFestival(event);
+    public boolean acceptEvent(Event event, BookingFormEntryPoint entryPoint) {
+        // Only supports new bookings for festivals
+        if (entryPoint != BookingFormEntryPoint.NEW_BOOKING) {
+            return false;
+        }
+        return event != null && FestivalType.isFestival(event);
     }
 
     @Override
@@ -24,7 +29,7 @@ public final class NKTOnlineFestivalBookingFormProvider implements BookingFormPr
     }
 
     @Override
-    public BookingForm createBookingForm(Event event, HasWorkingBookingProperties activity) {
+    public BookingForm createBookingForm(Event event, HasWorkingBookingProperties activity, BookingFormEntryPoint entryPoint) {
         return new NKTOnlineFestivalBookingForm(activity,
             new EventBookingFormSettingsBuilder(event)
                 .setEventHeader(new EventTitleHeader())
--- kbs-frontoffice-bookingform-nkt-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/bookingform/nkt/sttp/STTPBookingFormProvider.java
+++ kbs-frontoffice-bookingform-nkt-plugin/src/main/java/org/kadampabookings/kbs/frontoffice/bookingform/nkt/sttp/STTPBookingFormProvider.java
@@ -4,6 +4,7 @@ import dev.webfx.stack.orm.entity.Entities;
 import one.modality.base.shared.entities.Event;
 import one.modality.booking.client.workingbooking.HasWorkingBookingProperties;
 import one.modality.booking.frontoffice.bookingform.BookingForm;
+import one.modality.booking.frontoffice.bookingform.BookingFormEntryPoint;
 import one.modality.booking.frontoffice.bookingform.BookingFormProvider;
 import one.modality.event.frontoffice.activities.book.event.BookEventActivity;
 import one.modality.event.frontoffice.activities.book.event.EventBookingFormSettingsBuilder;
@@ -18,8 +19,12 @@ public class STTPBookingFormProvider implements BookingFormProvider {
     private static final int STTP_EVENT_TYPE_ID = 48;
 
     @Override
-    public boolean acceptEvent(Event event) {
-        return Entities.samePrimaryKey(event.getType(), STTP_EVENT_TYPE_ID);
+    public boolean acceptEvent(Event event, BookingFormEntryPoint entryPoint) {
+        // Only supports new bookings for STTP events
+        if (entryPoint != BookingFormEntryPoint.NEW_BOOKING) {
+            return false;
+        }
+        return event != null && Entities.samePrimaryKey(event.getType(), STTP_EVENT_TYPE_ID);
     }
 
     @Override
@@ -28,7 +33,7 @@ public class STTPBookingFormProvider implements BookingFormProvider {
     }
 
     @Override
-    public BookingForm createBookingForm(Event event, HasWorkingBookingProperties activity) {
+    public BookingForm createBookingForm(Event event, HasWorkingBookingProperties activity, BookingFormEntryPoint entryPoint) {
         return new RecurringEventBookingForm(event, (BookEventActivity) activity, new EventBookingFormSettingsBuilder(event)
             .setEventHeader(new MediaEventHeader(false))
             .build()
